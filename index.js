875ccc1c0b297ddd88670dbb181f6f06d6ebd862
Andriyan1 <42609561+Andriyan1@users.noreply.github.com>
2024-05-11 02:45:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //ton cinnect
-// const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-//     manifestUrl: 'https://andriyan1.github.io/tonconnect-manifest.json',
-//     buttonRootId: 'connect-wallet-btn'
-// });
+ const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+manifestUrl: 'https://andriyan1.github.io/tonconnect-manifest.json',
+    buttonRootId: 'connect-wallet-btn'
+});
 
 
 
