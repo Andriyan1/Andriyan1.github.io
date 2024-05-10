d8286fe6993bdcccca2e225087a5afc3da6a7aed
Andriyan1 <42609561+Andriyan1@users.noreply.github.com>
2024-05-10 15:26:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
 //telegram
 const tg = window.Telegram.WebApp;
 tg.expand();
+var myModalTransfer = document.getElementById('myModalTransfer');
+var btn = document.getElementById("btnBuy"); // Вибираємо кнопку з ідентифікатором "btnBuy"
+var span = document.querySelector(".close-Transfer"); // Вибираємо елемент з класом "close-Transfer"
+
+btn.onclick = function() {
+    myModalTransfer.style.display = "block"; // Показуємо модальне вікно при кліці на кнопку
+}
+
+span.onclick = function() {
+    myModalTransfer.style.display = "none"; // Ховаємо модальне вікно при кліці на елемент
+}
+
+window.onclick = function(event) {
+    if (event.target == myModalTransfer) {
+        myModalTransfer.style.display = "none";
+    }
+}
