76dcbab5cb76e5cbf5bfd21b54dc1287162dc476
Andriyan1 <42609561+Andriyan1@users.noreply.github.com>
2024-05-11 02:31:22 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+//ton cinnect
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+    manifestUrl: 'https://andriyan1.github.io/tonconnect-manifest.json',
+    buttonRootId: 'connect-wallet-btn'
+});
+
+
+
+//telegram
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+//block transfer
+var myModalTransfer = document.getElementById('myModalTransfer');
+var btn = document.getElementById("btnBuy");
+var spanTransfer = document.querySelector(".close-Transfer");
+
+var modalReceiv = document.querySelector('.modalReceiv');
+var BtnSend = document.getElementById("BtnReceive");
+var spanReceiv = document.querySelector(".close-Receiv");
+
+var currentModal = null; // зберігає посилання на поточне відображене модальне вікно
+
+btn.onclick = function() {
+    myModalTransfer.style.display = "block";
+    currentModal = myModalTransfer;
+}
+
+spanTransfer.onclick = function() {
+    myModalTransfer.style.display = "none";
+    currentModal = null;
+}
+
+BtnSend.onclick = function() {
+    modalReceiv.style.display = "block";
+    currentModal = modalReceiv;
+}
+
+spanReceiv.onclick = function() {
+    modalReceiv.style.display = "none";
+    currentModal = null;
+}
+
+window.onclick = function(event) {
+    if (event.target == myModalTransfer) {
+        myModalTransfer.style.display = "none";
+    }
+}
+
+function allowOnlyNumbers(event) {
+    // Дозволити використання клавіш "Backspace", "Delete", "Tab", "Enter" і "."
+    if ([46, 8, 9, 13, 110, 190].includes(event.keyCode) ||
+        // Дозволити клавіші від 0 до 9
+        (event.keyCode >= 48 && event.keyCode <= 57) ||
+        (event.keyCode >= 96 && event.keyCode <= 105)) {
+        return true;
+    }
+    // Інакше заборонити дію клавіші
+    return false;
+}
